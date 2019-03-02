cec041643206a1adc5044afface0548b790cee5a
Kamal Maged <Kamal_Maged@yahoo.com>
2019-03-01 22:48:47 -0500

    Testing

--- Test_AutoComplete/ViewController.swift
+++ Test_AutoComplete/ViewController.swift
@@ -1,20 +1,82 @@
 //
-//  ViewController.swift
-//  Test_AutoComplete
+//  OfferRideViewController.swift
+//  W8D2-Ride-Share
 //
-//  Created by Kamal Maged on 2019-03-01.
-//  Copyright © 2019 Kamal Maged. All rights reserved.
+//  Created by Pavel on 2019-02-28.
+//  Copyright © 2019 Pavel. All rights reserved.
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
-class ViewController: UIViewController {
-
+class OfferRideViewController: UIViewController, UISearchBarDelegate, LocateOnTheMap {
+    
+    var searchResultController:SearchResultsController!
+    var resultsArray = [String]()
+    var googleMapsView:GMSMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.googleMapsView = GMSMapView(frame: self.view.frame)
+        
+        self.view.addSubview(self.googleMapsView)
+        searchResultController = SearchResultsController()
+        searchResultController.delegate = self
+    }
+    
 
-
+    @IBAction func showSearchController(_ sender: Any) {
+        let searchController = UISearchController(searchResultsController: searchResultController)
+        searchController.searchBar.delegate = self
+        self.present(searchController, animated: true, completion: nil)
+    }
+    
+    func locateWithLongitude(lon: Double, andLatitude lat: Double, andTitle title: String) {
+        
+        DispatchQueue.main.async { () -> Void in
+            let position = CLLocationCoordinate2DMake(lat, lon)
+            let marker = GMSMarker(position: position)
+            
+            let camera  = GMSCameraPosition.camera(withLatitude: lat, longitude: lon, zoom: 10)
+            self.googleMapsView.camera = camera
+            
+            marker.title = title
+            marker.map = self.googleMapsView
+        }
+    }
+    func searchBar(_ searchBar: UISearchBar,
+                   textDidChange searchText: String){
+        
+        let placesClient = GMSPlacesClient()
+        
+        placesClient.autocompleteQuery(searchText, bounds: nil, filter: nil) { (results, error:Error?) -> Void in
+            self.resultsArray.removeAll()
+            if results == nil {
+                return
+            }
+            for result in results!{
+                if let result = result as? GMSAutocompletePrediction{
+                    self.resultsArray.append(result.attributedFullText.string)
+                }
+            }
+            self.searchResultController.reloadDataWithArray(array: self.resultsArray)
+        }
+    }
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
-
