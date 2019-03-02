6761ebf4088b789bf3fee2388d9167bfc9037f77
Kamal Maged <Kamal_Maged@yahoo.com>
2019-03-01 21:06:42 -0500

    Initial Commit

---
+++ Test_AutoComplete/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_AutoComplete
+//
+//  Created by Kamal Maged on 2019-03-01.
+//  Copyright © 2019 Kamal Maged. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
